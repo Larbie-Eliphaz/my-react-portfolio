25c2197cea3a4a4f0f3ed68f23a188640b4afaf7
Larbie-Eliphaz <eliphazlarbie3@gmail.com>
2025-11-07 03:28:18 +0000

    finish hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import './Hero.css';
+import React from "react";
+import profile from "../../assets/profile.jpg";
+import "./Hero.css";
 
 const Hero = () => {
-    return (
-        <div className="hero">
-            <h1>I'm Eliphaz Larbie, Fullstack Developer based in Accra, Ghana</h1>
-            <p>Your one-stop solution for tech innovations.</p>
-            <div className="hero-section">
-                <div className="hero-connect">Connect with me</div>
-                <div className="resume">My resume</div>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="hero">
+      <img src={profile} alt="Profile" className="profile-img" />
+      <h1 className="intro-text">
+        <span>I'm Eliphaz Larbie,</span> Fullstack Developer based in Accra, Ghana
+      </h1>
+      <p>Your one-stop solution for tech innovations.</p>
+      <div className="hero-actions">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
