57aabbf908d84dbc72f414a7a1458082d72234a2
Larbie-Eliphaz <eliphazlarbie3@gmail.com>
2025-11-07 20:54:43 +0000

    finish About me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar.jsx';
 import Hero from './components/hero/Hero.jsx';
+import About from './components/about/About.jsx';
+
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import './About.css'
+import Matpic from '../../assets/matpic.JPG';
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="title">
+        <h2>About Me</h2>
+      </div>
+      <div className="description">
+        <div className="left-pane">
+          <img src={Matpic} alt="My image" className="img" />
+        </div>
+        <div className="right-pane">
+          <div className="p">
+            <p className="">
+              Hello! I'm Eliphaz Larbie, a passionate web developer with a knack
+              for creating dynamic and responsive web applications. With a
+              strong foundation in HTML, CSS, and JavaScript.
+            </p>
+            <p className="">
+              I enjoy turning complex problems into simple, beautiful, and
+              intuitive designs. When I'm not coding, you'll find me exploring
+              the latest tech trends or hiking in the great outdoors.
+            </p>
+          </div>
+          <div className="skills">
+            <div className="skill"><p>HTML & CSS</p> <hr style={{width:"50%"}}/></div>
+            <div className="skill"><p>JavaScript</p> <hr style={{width:"60%"}}/></div>
+            <div className="skill"><p>React JS</p> <hr style={{width:"70%"}}/></div>
+            <div className="skill"><p>Node Backend</p> <hr style={{width:"80%"}}/></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
