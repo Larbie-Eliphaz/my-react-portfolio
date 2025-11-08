ea3e57ba088d76c7c289111dfa6bba29dd673c55
Larbie-Eliphaz <eliphazlarbie3@gmail.com>
2025-11-08 10:52:24 +0000

    finish services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import Navbar from './components/navbar/Navbar.jsx';
-import Hero from './components/hero/Hero.jsx';
-import About from './components/about/About.jsx';
+import React from "react";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Hero from "./components/hero/Hero.jsx";
+import About from "./components/about/About.jsx";
+import Services from "./components/services/Services.jsx";
 
 const App = () => {
   return (
@@ -9,8 +10,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Services />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Services.css";
+
+const Services = () => {
+  return (
+    <div className="services">
+      <h1>Services</h1>
+      <div className="container">
+        <div className="service">
+          <h3>1</h3>
+          <h2>Name</h2>
+          <p>description of the service i render</p>
+          <p className="read-more">Read More...</p>
+        </div>
+        <div className="service">
+          <h3>2</h3>
+          <h2>Name</h2>
+          <p>description of the service i render</p>
+          <p className="read-more">Read More...</p>
+        </div>
+        <div className="service">
+          <h3>3</h3>
+          <h2>Name</h2>
+          <p>description of the service i render</p>
+          <p className="read-more">Read More...</p>
+        </div>
+        <div className="service">
+          <h3>1</h3>
+          <h2>Name</h2>
+          <p>description of the service i render</p>
+          <p className="read-more">Read More...</p>
+        </div>
+        <div className="service">
+          <h3>2</h3>
+          <h2>Name</h2>
+          <p>description of the service i render</p>
+          <p className="read-more">Read More...</p>
+        </div>
+        <div className="service">
+          <h3>3</h3>
+          <h2>Name</h2>
+          <p>description of the service i render</p>
+          <p className="read-more">Read More...</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
