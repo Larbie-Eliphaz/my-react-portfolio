b39712006a1b232d65835d8a1aea820758694bea
Larbie-Eliphaz <eliphazlarbie3@gmail.com>
2025-11-10 15:04:48 +0000

    finish the projects section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/navbar/Navbar.jsx";
 import Hero from "./components/hero/Hero.jsx";
 import About from "./components/about/About.jsx";
 import Services from "./components/services/Services.jsx";
+import Projects from "./components/projects/Projects.jsx";
+import Contact from "./components/contact/Contact.jsx";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Hero />
       <About />
       <Services />
+      <Projects />
+      <Contact />
     </div>
   );
 };
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import coffeeMachine from "../../assets/coffeemachine.jpg";
+import ecommerce from "../../assets/e-commerce .jpg";
+import todo from "../../assets/todo.png";
+import "./Projects.css";
+
+const Projects = () => {
+  return (
+    <section class="projects" id="projects">
+      <h1>Featured Projects</h1>
+      <hr />
+
+      <div class="card">
+        <img src={todo} alt="card image" class="card-img-top" />
+        <div class="card-body">
+          <h5 class="card-title">Todo-List App</h5>
+          <p class="card-text">
+            The To-do list app helps users to organize their tasks and stay
+            productive by allowing them to create, edit, and prioritize their
+            To-do list. With a user-friendly interface, users can easily add
+            tasks, set deadlines, and mark them as complete.
+          </p>
+          <a href="./old projects/todo.html" target="_blank">View Project</a>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src={ecommerce} class="card-img-top" alt="card image" />
+        <div class="card-body">
+          <h5 class="card-title">E-commerce website</h5>
+          <p class="card-text">
+            The E-commerce website is an interactive Online shop that allows
+            users to buy items from the shop. With various products available,
+            users can choose from a wide range of products in different
+            categories.
+          </p>
+          <a href="./ecommerce.html" target="_blank">View Project</a>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src={coffeeMachine} class="card-img-top" alt="card image" />
+        <div class="card-body">
+          <h5 class="card-title">Virtual Coffee Machine</h5>
+          <p class="card-text">
+            The virtual coffee machine is an interactive simulation that allows
+            users design and brew their perfect cup of coffee. With various
+            coffee options, users can experiment with different flavour and
+            recipes.
+          </p>
+          <a href="./coffeeMachine.html" target="_blank">View Project</a>
+        </div>
+      </div>
+
+      <a href="./new_port_projects.html" target="_blank">
+        All Projects ➡
+      </a>
+    </section>
+  );
+};
+
+export default Projects;
